f28d5dd79b1dca92af0075fdfa316529132de92b
WNCRY <dimacelujkin@gmail.com>
2024-01-09 05:28:42 +0200

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { About, Home, SignIn, SignUp } from './pages';
+
 function App() {
-  return <div className="">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<SignIn />}></Route>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
