f33e41db219851c594e88c92ef78c7aa5be663cc
WNCRY <dimacelujkin@gmail.com>
2024-01-09 05:54:04 +0200

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { About, Home, SignIn, SignUp } from './pages';
+import { Header } from './components';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<SignIn />}></Route>
-        <Route path="/" element={<SignUp />}></Route>
-        <Route path="/" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </BrowserRouter>
   );
